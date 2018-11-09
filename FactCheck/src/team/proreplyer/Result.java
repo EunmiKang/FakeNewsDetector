171d6b0e3a66b774a07016dc28e0afdb3350d021
EunmiKang <hv19962000@gmail.com>
2018-11-09 17:00:02 +0900

    Result.java setResultFlag 메소드 추가 && result_flag 초기화값 변경

--- FactCheck/src/team/proreplyer/Result.java
+++ FactCheck/src/team/proreplyer/Result.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONArray;
 
 public class Result {
 	public JSONArray relatedData_array = new JSONArray();
+	public int result_flag = 4;
 	public String result;
 	public String[] result_string = {"가짜뉴스가 아닙니다!", "가짜뉴스가 아닐 확률이 높습니다!", "가짜뉴스입니다!", "가짜뉴스일 확률이 높습니다!", "판단 유보"};
 	
@@ -14,5 +15,9 @@ public class Result {
 	public Result(JSONArray relatedData_array, String result) {
 		this.relatedData_array = relatedData_array;
 		this.result = result;
+	}
+	
+	public void setResultFlag(int result_flag) {
+		this.result_flag = result_flag;
 	}
 }
