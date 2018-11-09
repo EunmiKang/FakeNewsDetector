4365a8041f49bf3b7fa9a46ea430c0681cb07179
EunmiKang <hv19962000@gmail.com>
2018-11-09 17:02:44 +0900

    SentenceInfo.java NLPtoSentenceInfo 메소드 수정

--- FactCheck/src/team/proreplyer/SentenceInfo.java
+++ FactCheck/src/team/proreplyer/SentenceInfo.java
@@ -36,55 +36,69 @@ public class SentenceInfo {
 		this.link = link;
 	}
 	
-	public SentenceInfo NLPtoSentenceInfo(String sentence, JSONArray NLP_sentence){
-		JSONObject sentence_obj = (JSONObject)NLP_sentence.get(0);
+	public SentenceInfo NLPtoSentenceInfo(String sentence, JSONArray NLP_sentence) {
+		JSONObject sentence_obj = (JSONObject) NLP_sentence.get(0);
 		JSONArray srlArray = (JSONArray) sentence_obj.get("SRL");
-		JSONArray dependencyArray = (JSONArray) sentence_obj.get("dependency");
-		JSONObject srlObject = (JSONObject) srlArray.get(0);
-		String verb = srlObject.get("verb").toString();
-		JSONArray argument = (JSONArray) srlObject.get("argument");
-		String sbj = "null";
-		String obj = "null";
-		String tmp = "null";
-		String location = "null";
-		String neg = "no"; 
-		String adv = "null";
-		String cmp = "null";
-		
-		for(Object word : argument){
-			JSONObject temp = (JSONObject) word;
-			if(temp.get("type").equals("ARG0")){
-				sbj = temp.get("text").toString();
+		if (srlArray.size() > 0) {			
+			JSONArray dependencyArray = (JSONArray) sentence_obj.get("dependency");
+			JSONObject srlObject = (JSONObject) srlArray.get(srlArray.size() - 1);
+			String verb = srlObject.get("verb").toString();
+			JSONArray argument = (JSONArray) srlObject.get("argument");
+			String sbj = "null";
+			String obj = "null";
+			String tmp = "null";
+			String location = "null";
+			String neg = "no";
+			String adv = "null";
+			String cmp = "null";
+
+			for (Object word : argument) {
+				JSONObject temp = (JSONObject) word;
+				if (temp.get("type").equals("ARG0")) {
+					sbj = temp.get("text").toString();
+				} else if (temp.get("type").equals("ARG1")) {
+					obj = temp.get("text").toString();
+				} else if (temp.get("type").equals("ARGM-TMP")) {
+					tmp = temp.get("text").toString();
+				} else if (temp.get("type").equals("ARGM-LOC")) {
+					location = temp.get("text").toString();
+				} else if (temp.get("type").equals("ARGM-NEG")) {
+					neg = temp.get("text").toString();
+				}
 			}
-			else if(temp.get("type").equals("ARG1")){
-				obj = temp.get("text").toString();
+
+			for (Object word : dependencyArray) {
+				JSONObject temp = (JSONObject) word;
+				if (sbj.equals("null")) {
+					if (temp.get("label").toString().contains("SBJ")) { // 주어
+						sbj = temp.get("text").toString();
+						sbj = sbj.substring(0, sbj.length() - 1); // 뒤에 조사 떼기
+						if(obj.contains(sbj)) {
+							sbj = obj;
+							obj = "null";
+						}
+					}
+				}
+				if (temp.get("label").toString().contains("AJT") || temp.get("label").toString().contains("AP")) {// 부사어
+					adv = temp.get("text").toString();
+				} else if (temp.get("label").toString().contains("CMP")) {// 보어
+					cmp = temp.get("text").toString();
+				}
 			}
-			else if(temp.get("type").equals("ARGM-TMP")){
-				tmp = temp.get("text").toString();
+			
+			if(tmp.equals("null")) {
+				JSONArray NEArray = (JSONArray) sentence_obj.get("NE");
+				
+				for (Object word : NEArray) {
+					JSONObject temp = (JSONObject) word;
+					if (temp.get("type").toString().contains("DT")) {
+						tmp = temp.get("text").toString();
+					}
+				}
 			}
-			else if(temp.get("type").equals("ARGM-LOC")){
-				location = temp.get("text").toString();
-			}
-			else if(temp.get("type").equals("ARGM-NEG")){
-				neg = temp.get("text").toString();
-			}
+
+			return new SentenceInfo(sbj, obj, verb, tmp, location, neg, adv, cmp, sentence, link);
 		}
-	
-		for(Object word : dependencyArray){
-			JSONObject temp = (JSONObject) word;
-			if(temp.get("label").toString().contains("AJT")){//부사어
-				adv = temp.get("text").toString();
-			}
-			else if(temp.get("label").toString().contains("CMP")){//보어
-				cmp = temp.get("text").toString();
-			}
-		}
-		
-		if(sbj.equals("null")){
-			sbj = obj;
-			obj = "null";
-		}
-		
-		return new SentenceInfo(sbj, obj, verb, tmp, location, neg, adv, cmp, sentence, link);
+		return null;
 	}
 }
